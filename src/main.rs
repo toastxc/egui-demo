1fc5601e6cf5672a3bbddf1f49c1d6c83c94f909
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-01-12 10:52:47 +0800

    upload

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use crate::structures::{Progress, SearchFields};
 use eframe::{HardwareAcceleration, Renderer};
-
-use crate::engine::methods::bazaar::ProfitInfo;
+use reywen_http::engines::hyper::Hyper;
 use std::sync::{Arc, RwLock};
 use tokio::runtime::Runtime;
 
-pub mod engine;
 pub mod process;
-pub mod structures;
+
 pub mod view;
 
-fn main() -> Result<(), eframe::Error> {
+static MIN_WIDTH: f32 = 300.0;
+static DEFAULT_WIDTH: f32 = 480.0;
+static MIN_HEIGHT: f32 = 480.0;
+static DEFAULT_HEIGHT: f32 = 480.0;
+
+
+
+fn main() {
     env_logger::init();
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([480.0, 480.0])
-            .with_min_inner_size([480.0, 300.0])
+            .with_inner_size([DEFAULT_WIDTH, DEFAULT_HEIGHT])
+            .with_min_inner_size([MIN_WIDTH, MIN_HEIGHT])
             .with_transparent(true),
         vsync: true,
         hardware_acceleration: HardwareAcceleration::Preferred,
@@ -25,44 +29,39 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
 
-    eframe::run_native(
-        "Bazaar",
-        options,
-        Box::new(|_cc| Box::from(MyApp::from_theme(_cc.egui_ctx.style().visuals.dark_mode))),
-    )?;
-
-    Ok(())
+    eframe::run_native("App", options, Box::new(|_cc| Box::from(App::default()))).unwrap();
 }
+
+pub type Guard<T> = Arc<RwLock<T>>;
 
 #[derive(Debug, Clone)]
-struct MyApp {
-    pub search: SearchFields,
+struct App {
+    // this field is only accessed in 'sync' code
+    pub button1_text: String,
+
+    // this field is accessed in 'sync' and 'async' code
+    // since multiple threads can access the data it is protected
+    pub label1_text: Guard<String>,
+
+    // this will let users know if something is happening in the background
+    pub spin: Guard<bool>,
+
+    // this is out tokio thread runtime, it is never written to and as such only needs
+    // an Arc<T>, instead of Arc<RwLock<T>>
     pub runtime: Arc<Runtime>,
-    pub original_data: Arc<RwLock<Vec<ProfitInfo>>>,
-    pub processed_data: Vec<ProfitInfo>,
-    pub progress: Arc<RwLock<Progress>>,
-    pub is_dark: bool,
+
+    // this is the http engine used for making requests, I've selected my own but you can choose any!
+    pub engine: Arc<Hyper>,
 }
 
-impl MyApp {
-    pub fn from_theme(theme: bool) -> Self {
-        Self::default().set_theme(theme)
-    }
-    pub fn set_theme(&mut self, is_dark: bool) -> Self {
-        self.is_dark = is_dark;
-        self.clone()
-    }
-}
-
-impl Default for MyApp {
+impl Default for App {
     fn default() -> Self {
         Self {
+            button1_text: String::from("Click to run a HTTP request"),
+            label1_text: Arc::new(Default::default()),
+            spin: Arc::new(Default::default()),
             runtime: Arc::new(Runtime::new().unwrap()),
-            search: Default::default(),
-            processed_data: vec![],
-            original_data: Arc::new(RwLock::new(Vec::new())),
-            progress: Arc::new(RwLock::new(Progress::default())),
-            is_dark: true,
+            engine: Arc::new(Default::default()),
         }
     }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,66 +1,37 @@
-use crate::{structures::SortBy, MyApp};
-
-use crate::engine::methods::bazaar::ProfitInfo;
-use crate::engine::Hypixel;
+use crate::App;
+use reywen_http::engines::hyper::Method;
 use std::sync::Arc;
 
-impl MyApp {
-    pub fn click_add_find_button(&mut self) {
-        let progress = Arc::clone(&self.progress);
+impl App {
+    // this function calls the requests the internet
+    pub fn request(&mut self) {
+        // all fields that are a part of self that you use MUST be called here
         let runtime = Arc::clone(&self.runtime);
-        let original_data = Arc::clone(&self.original_data);
+        let engine = Arc::clone(&self.engine);
+        let label_text = Arc::clone(&self.label1_text);
+        let spin = Arc::clone(&self.spin);
 
+        // async code can only be executed in this block
         runtime.spawn(async move {
-            progress.write().unwrap().set(15.0, 470.0, 0.5, true);
-            *original_data.write().unwrap() = Hypixel::new().bazaar_profit().await.unwrap_or_default();
-            progress.write().unwrap().set_default();
+            // set the spinner to appear
+            *spin.write().unwrap() = true;
+            // http request - turn into string
+            let str = String::from_utf8(
+                engine
+                    .request_raw(Method::GET, "https://repo.toastxc.xyz/test", None)
+                    .await
+                    .unwrap_or("Oops, Something went wrong...".to_string().into_bytes()),
+            )
+            .unwrap();
+            // save data
+            *label_text.write().unwrap() = str;
+
+            // remove spinner
+            *spin.write().unwrap() = false;
         });
     }
 
-    pub fn calculate(&mut self) {
-        let mut polled_data: Vec<ProfitInfo> = self
-            .original_data
-            .clone()
-            .read()
-            .unwrap()
-            .clone()
-            .into_iter()
-            .filter(|a| {
-                // basic profitability filtering
-                a.bazaar_buy_price >= self.search.min_buy_val as f32
-                    && a.bazaar_sell_price >= self.search.min_sell_val as  f32
-                    && a.weekly_buy_orders != 0
-                    && a.weekly_sell_orders != 0
-                    && a.flip_value != 0.0
-                    && a.bazaar_buy_price > a.bazaar_sell_price
-                // optional search fields
-                && { a.weekly_buy_orders + a.weekly_sell_orders } as i64
-                     > self.search.order_total as i64
-                && a.flip_value > self.search.profit as f32
-                && a.item_name.contains::<&String>(&self.search.name.to_ascii_uppercase().chars()
-                    .map(|c| if c == '_' { ' ' } else { c })
-                    .collect())
-                    // special case
-                && self.search.filter.field.check3(&a.item_name, &self.search.filter.invert)
-            })
-            .collect();
-
-        match self.search.sort_by.sort_by {
-            SortBy::FlipValue => polled_data.sort_by_key(|a| a.flip_value as i32),
-            SortBy::WeeklyOrders => {
-                polled_data.sort_by_key(|a| (a.weekly_sell_orders, a.weekly_buy_orders))
-            }
-            SortBy::Az => polled_data.sort_by_key(|a| a.item_name.clone()),
-            SortBy::FlipPercentage => polled_data.sort_by_key(|a| a.flip_percentage as i32),
-        }
-
-        match (self.search.sort_by.inverted, &self.search.sort_by.sort_by) {
-            (true, SortBy::Az) => polled_data.reverse(),
-            (false, SortBy::Az) => {}
-
-            (true, _) => {}
-            (false, _) => polled_data.reverse(),
-        }
-        self.processed_data = polled_data;
+    pub fn do_stuff(&mut self) {
+        // this function, and any function called by the update function will run once per frame, be careful!
     }
 }
